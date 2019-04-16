01d7197ed9fca2bd1fc02c95cb02ba386bf89608
Roberto Atanasio <roberto.atanasio.pl@gmail.com>
2019-04-15 22:28:00 -0300

    adicionando EntryFoemComponent'

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
